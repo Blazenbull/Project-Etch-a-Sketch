7736ad2364e421ded0e380aa8e7c8198b51e950d
blazenbull <164746448+Blazenbull@users.noreply.github.com>
2025-02-05 18:23:17 +0800

    hover color change function added to col for loop

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 //to-do-next make divs of rows to print out to complete 16x16
 console.log("hello world")
 const container1= document.querySelector(".container1");
+const title=document.querySelector("#title");
 let rows=16;
 let cols=16;
+
+title.style.textAlign="center"
 
 for(j=0; j<rows; j++){
 const row= document.createElement("div");
@@ -16,12 +19,13 @@ console.log("row");
   for (i=0; i<cols; i++){
   const col = document.createElement("div");
   col.className="col";
-  
   col.style.border="thick solid black";
   col.style.width= "50px"
   col.style.height="50px"
+  col.addEventListener("mouseenter", (e) => {
+    col.style.backgroundColor="green";
+  });  
   row.appendChild(col);
   console.log(col)
-
   }
 }
