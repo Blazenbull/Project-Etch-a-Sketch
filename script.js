9be250358f1c8212de0b1df16b1acc7735019835
blazenbull <164746448+Blazenbull@users.noreply.github.com>
2025-02-07 15:33:14 +0800

    able to make different grid sizes but keep the same resolution of the whole container

--- script.js
+++ script.js
@@ -1,33 +1,45 @@
 /* to-do-next
-1. work on removing older grids when pressing new grid button more than once
-2. to make board resolution a fixed size
+1. randomise the color of the grids when moused over again
+2. darken the grid color when crossed over more than once
 */
 console.log("hello world")
 let container1= document.querySelector("#container1");
+container1.style.border="thick solid green"
+container1.style.height ="960px"
+container1.style.width ="960px"
+container1.style.margin="0 auto"//centers the grid to center of webpage
+
 const title=document.querySelector("#title");
-
-let rows=16;
-let cols=16;
-
 title.style.textAlign="center"
-const buttonsRow=document.createElement("div")
+
+let rows=16;//default rows
+let cols=16;//default columns
+
+
+const buttonsRow=document.createElement("div")//create area for buttons
 title.appendChild(buttonsRow);
 
-console.log(rows)
-const gridButton=document.createElement("button");
+const gridButton=document.createElement("button");//create actual button
 buttonsRow.appendChild(gridButton);
 gridButton.textContent="Grid Size";
 
+//grid button steps
 gridButton.addEventListener ("click", (e)=>{
 
   let size=prompt("How large of a grid would you like? (between 1x1 to 100x100)");
   if(size>=1 && size<=100){console.log(size);
-  container1.remove()
+  container1.remove()//remove old grid
   rows=size;
   cols=size
 
   let container2=document.createElement("div")
+  container2.style.border="thick solid green";
+  container2.style.height ="960px";
+  container2.style.width ="960px";
+  
+  container2.style.margin="0 auto";
   document.body.appendChild(container2)
+  
   createGrid(container2)
   container1=container2;
 } else{
@@ -40,9 +52,11 @@ function createGrid(x){
 for(j=0; j<rows; j++){
 const row= document.createElement("div");
 row.className="row";
-row.style.display="flex"
-row.style.flexDirection="row"
-row.style.justifyContent="center"
+row.style.display="flex";
+row.style.flex="1 1 auto";
+rowHeight=960/rows;
+row.style.height=`${rowHeight}px`;
+row.style.justifyContent="center";
 
 x.appendChild(row);
 
@@ -50,13 +64,13 @@ x.appendChild(row);
   const col = document.createElement("div");
   col.className="col";
   col.style.border="thick solid black";
-  col.style.width= "50px"
-  col.style.height="50px"
+  colWidth=960/cols;
+  col.style.width=`${colWidth}px`;
+
   col.addEventListener("mouseenter", (e) => {
     col.style.backgroundColor="green";
   });  
   row.appendChild(col);
-  console.log(col)
   }
 }
 }
